f30c9ba2991e0a2775142a78f6d994bb484b564c
bignoseben <jackwanamaker49@gmail.com>
2024-07-26 10:58:17 -0400

    Added some textformatter shenans

--- src/main/java/com/jammin/testjavafx/TextFieldFilterTest.java
+++ src/main/java/com/jammin/testjavafx/TextFieldFilterTest.java
@@ -1,2 +1,31 @@
-package com.jammin.testjavafx;public class TextFieldFilterTest {
+package com.jammin.testjavafx;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.stage.Stage;
+import javafx.util.converter.IntegerStringConverter;
+
+public class TextFieldFilterTest extends Application {
+
+    public void start(Stage primaryStage) {
+
+
+    }
+
+    private TextField createTextField() {
+        TextField myTextField = new TextField("My Test TextField");
+        myTextField.setAlignment(Pos.CENTER);
+        myTextField.setOnAction((evt) -> System.out.println("Did it work? :-)"));
+
+        TextFormatter <Integer> myTextFormatter = new TextFormatter<>(new IntegerStringConverter(), 60, this::filter);
+        myTextField.setTextFormatter(myTextFormatter);
+
+        return myTextField;
+    }
+
+    private TextFormatter.Change filter(TextFormatter.Change change){
+
+    }
 }
