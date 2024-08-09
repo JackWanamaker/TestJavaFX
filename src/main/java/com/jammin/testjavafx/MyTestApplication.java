162d270ac73205774e68cb744d16f609e154656c
Ben Summer <jackwanamaker49@gmail.com>
2024-08-09 11:47:31 -0600

    Deleted old arc stuff. Will not test WebView functionality with svg images

--- src/main/java/com/jammin/testjavafx/MyTestApplication.java
+++ src/main/java/com/jammin/testjavafx/MyTestApplication.java
@@ -10,24 +10,13 @@ import javafx.stage.Stage;
 
 public class MyTestApplication extends Application {
     public void start(Stage PrimaryStage){
+
+
         StackPane myStackPane = new StackPane();
 
         Scene scene = new Scene(myStackPane,200,200);
 
-        createArc(myStackPane);
-
         PrimaryStage.setScene(scene);
         PrimaryStage.show();
-    }
-
-    //Constructor for my Arc
-    public void createArc(StackPane myStackPane){
-        Arc myArc = new Arc(100,100,100,100,0,360);
-        myArc.setFill(Color.GREEN);
-        myArc.setType(ArcType.ROUND);
-        myStackPane.getChildren().add(myArc);
-
-        myArc.radiusXProperty().bind(myArc.getParent().getScene().heightProperty().divide(2.5));
-        myArc.radiusYProperty().bind(myArc.getParent().getScene().heightProperty().divide(2.5));
     }
 }
