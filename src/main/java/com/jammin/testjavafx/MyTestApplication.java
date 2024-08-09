465beed413b428d4b501a73bd22074ae475f6034
Ben Summer <jackwanamaker49@gmail.com>
2024-08-09 16:39:31 -0600

    Created an svg image that can display in JavaFX

--- src/main/java/com/jammin/testjavafx/MyTestApplication.java
+++ src/main/java/com/jammin/testjavafx/MyTestApplication.java
@@ -3,18 +3,19 @@ package com.jammin.testjavafx;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Arc;
-import javafx.scene.shape.ArcType;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class MyTestApplication extends Application {
     public void start(Stage PrimaryStage){
+        WebView myWebView = new WebView();
+        String svgPath = getClass().getResource("/img/svg_logo.svg").toExternalForm();
+        String svgContent = "<html><body><img src = '" + svgPath + "'></body></html>";
+        myWebView.getEngine().loadContent(svgContent);
 
+        StackPane myStackPane = new StackPane(myWebView);
 
-        StackPane myStackPane = new StackPane();
-
-        Scene scene = new Scene(myStackPane,200,200);
+        Scene scene = new Scene(myStackPane,500,500);
 
         PrimaryStage.setScene(scene);
         PrimaryStage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.jammin.testjavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
 
     opens com.jammin.testjavafx to javafx.fxml;
