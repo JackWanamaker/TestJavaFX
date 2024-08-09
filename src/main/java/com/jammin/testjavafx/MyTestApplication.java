b5fdcd4852c68307ca03cf789188e90681768b4d
Ben Summer <jackwanamaker49@gmail.com>
2024-08-09 09:30:44 -0600

    Created an example of a scaling arc as the size of the scene in my application changes. Deleted old files

---
+++ src/main/java/com/jammin/testjavafx/MyTestApplication.java
@@ -0,0 +1,33 @@
+package com.jammin.testjavafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.stage.Stage;
+
+public class MyTestApplication extends Application {
+    public void start(Stage PrimaryStage){
+        StackPane myStackPane = new StackPane();
+
+        Scene scene = new Scene(myStackPane);
+
+        createArc(myStackPane);
+
+        PrimaryStage.setScene(scene);
+        PrimaryStage.show();
+    }
+
+    //Constructor for my Arc
+    public void createArc(StackPane myStackPane){
+        Arc myArc = new Arc(100,100,100,100,0,360);
+        myArc.setFill(Color.GREEN);
+        myArc.setType(ArcType.ROUND);
+        myStackPane.getChildren().add(myArc);
+
+        myArc.radiusXProperty().bind(myArc.getParent().getScene().widthProperty().divide(2));
+        myArc.radiusYProperty().bind(myArc.getParent().getScene().heightProperty().divide(2));
+    }
+}
