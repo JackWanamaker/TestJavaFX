7d8050ddfb72c039734bf8361a18fab4156d6272
bignoseben <jackwanamaker49@gmail.com>
2024-08-22 08:38:21 -0400

    Wow! Created some static Strings and manipulated them with ChangeListeners to change the size of the SVG image. Right on

--- src/main/java/com/jammin/testjavafx/HelloApplication.java
+++ src/main/java/com/jammin/testjavafx/HelloApplication.java
@@ -1,6 +1,8 @@
 package com.jammin.testjavafx;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.*;
 import javafx.scene.control.*;
@@ -9,66 +11,44 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import javafx.util.converter.IntegerStringConverter;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
-    private static final double R = 50;
-    private static final double S = R * 2 + 10;
 
     @Override
     public void start(Stage stage) throws IOException {
-        Arc timeArc = createArc();
-        Pane arcPane = createArcPane(timeArc);
-        TextField timeField = createTimeField(timeArc);
+        WebView myWebView = new WebView();
+        myWebView.getEngine().loadContent(SVGString.getString());
 
-        VBox layout = new VBox(10, new Label("Enter Timer Length (1-60min)"), timeField, arcPane);
-        layout.setPadding(new Insets(10));
+        StackPane myStackPane = new StackPane(myWebView);
 
-        stage.setScene(new Scene(layout));
-        stage.show();
-    }
+        Scene myScene = new Scene(myStackPane);
 
-    private TextField createTimeField(Arc timeArc) {
-        TextField timeField = new TextField();
+        stage.setScene(myScene);
 
-        TextFormatter<Integer> timeFormatter = new TextFormatter<>(
-                new IntegerStringConverter(),
-                60,
-                this::filter
-        );
-        timeField.setTextFormatter(timeFormatter);
-        timeField.setOnAction(_ -> timeArc.setLength(timeFormatter.getValue() != null ? timeFormatter.getValue() * 6 : 0));
-        return timeField;
-    }
-
-    private TextFormatter.Change filter(TextFormatter.Change change) {
-        try {
-            if (change.getControlNewText().isEmpty()) {
-                return change;
+        myScene.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                SVGString.changeSVGHeight(t1.intValue());
+                myWebView.getEngine().loadContent(SVGString.getString());
             }
+        });
 
-            int val = Integer.parseInt(change.getControlNewText());
+        myScene.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                SVGString.changeSVGWidth(t1.intValue());
+                myWebView.getEngine().loadContent(SVGString.getString());
+            }
+        });
 
-            return val >= 0 && val <= 60 ? change : null;
-        } catch (NumberFormatException e) {
-            return null;
-        }
-    }
+        stage.setAlwaysOnTop(true);
 
-    private static Arc createArc() {
-        Arc timeArc = new Arc(R, R, R, R, 0, 360);
-        timeArc.setStroke(Color.MEDIUMSLATEBLUE);
-        timeArc.setFill(null);
-        timeArc.setStrokeWidth(5);
-        timeArc.setStrokeLineCap(StrokeLineCap.ROUND);
+        stage.show();
 
-        return timeArc;
-    }
-
-    private static Pane createArcPane(Arc timeArc) {
-        Rectangle background = new Rectangle(S, S, Color.TRANSPARENT);
-
-        return new StackPane(new Group(background, timeArc));
     }
 }
